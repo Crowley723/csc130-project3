1abc01b55643beea6741e7dd70e62ac14389e901
Crowley723 <littlehill723@gmail.com>
2023-10-21 21:16:58 -0700

    added dummy welcome page

--- index.php
+++ index.php
@@ -1,4 +1,79 @@
 <!DOCTYPE html>
-<html>
-    
+<style>
+    body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+}
+
+.products {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin: 20px;
+}
+
+.product {
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+    max-width: 300px;
+}
+
+.product img {
+    max-width: 100%;
+}
+
+button {
+    background-color: #333;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+button:hover {
+    background-color: #555;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+}
+
+</style>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dummy Storefront</title>
+    <?php include "./header.php" ?>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Dummy Storefront</h1>
+        <p>Your one-stop shop for all things tech!</p>
+    </header>
+        
+    <footer>
+        <p>&copy; 2023 Dummy Storefront. All rights reserved.</p>
+    </footer>
+</body>
 </html>
