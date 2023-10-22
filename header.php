f68b2ee482151c3e7e34f9824a51d43203f18a2f
Crowley723 <littlehill723@gmail.com>
2023-10-21 23:04:38 -0700

    fixed path for login page

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <div class="topnav" id="myTopnav">
     <a href="/"<?php if($_SERVER['REQUEST_URI'] == "/index.php" or $_SERVER['REQUEST_URI'] == "/"){echo " class=\"active\"";} ?>>Welcome</a>
     <a href="/shop.php"<?php if($_SERVER['REQUEST_URI'] == "/shop.php" or $_SERVER['REQUEST_URI'] == "/shop.php"){echo " class=\"active\"";} ?>>Shop</a>
-    <a href="/login.php"<?php if($_SERVER['REQUEST_URI'] == "/login.php"){echo " class=\"split active\"";}else echo " class=\"split\"" ?>>Login</a>
+    <a href="/account/login.php"<?php if($_SERVER['REQUEST_URI'] == "/account/login.php"){echo " class=\"split active\"";}else echo " class=\"split\"" ?>>Login</a>
     <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="showHamburgerMenu()">&#9776;</a>
     <script>
       function showHamburgerMenu() {
