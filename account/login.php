e6022b5a909800121177015ad7717322d1d82e28
Crowley723 <littlehill723@gmail.com>
2023-10-23 12:02:06 -0700

    fix css class for login-form

--- account/login.php
+++ account/login.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="/design.css">
-        <?php include "../header.php" ?>
-        
-        <title>Login</title>
-    </head>
-    <script>
+<head>
+    <link rel="stylesheet" href="/design.css">
+    <?php include "../header.php" ?>
+    
+    <title>Login</title>
+</head>
+<script>
     document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+        event.preventDefault();
 
-    // Get the values from the form
-    var uid = document.getElementById('uid').value;
-    var password = document.getElementById('password').value;
+        // Get the values from the form
+        var uid = document.getElementById('uid').value;
+        var password = document.getElementById('password').value;
 
-    // Send the data to the backend (for example, using fetch or XMLHttpRequest)
-    fetch('/handleLogin.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            uid: uid,
-            password: password
+        // Send the data to the backend (for example, using fetch or XMLHttpRequest)
+        fetch('/handleLogin.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                uid: uid,
+                password: password
+            })
         })
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response from the backend
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
-});
-    </script>
+</script>
     <body>
     <h1>Login Page</h1>
-    <form id="loginForm">
+    <form id="login-form">
         <div>
             <label for="uid">Username:</label>
             <input type="text" id="uid" name="uid" required>
