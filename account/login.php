1322d2d75924d48c1464062fb42b3a1cf3dc8edc
Crowley723 <littlehill723@gmail.com>
2023-10-23 12:08:51 -0700

    updated login styles - center the login box

--- account/login.php
+++ account/login.php
@@ -37,16 +37,18 @@
 </script>
     <body>
     <h1>Login Page</h1>
-    <form id="login-form">
-        <div>
-            <label for="uid">Username:</label>
-            <input type="text" id="uid" name="uid" required>
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit">Login</button>
-    </form>
+    <div class="login-box">
+        <form id="login-form">
+            <div>
+                <label for="uid">Username:</label>
+                <input type="text" id="uid" name="uid" required>
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
 </body>
 </html>
