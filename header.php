566f2f14511fb0e5fcfb6db46da4c6dc3ad5598a
Brynn Crowley <littlehill723@gmail.com>
2023-10-23 17:22:45 -0700

    moved the login modal outside of nav bar

--- header.php
+++ header.php
@@ -17,38 +17,39 @@
         }
       }
     </script>
-    <div class="login">
-    <div id="id01" class="modal">
-      <form class="modal-content animate" action="/action_page.php" method="post">
-        <div class="imgcontainer">
-          <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">%times;</span>
-          <img src="login_silhouette.png" alt="Avatar" class="avatar">
-        </div>
-        <div class="container">
-          <label for="username"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="username" required>
-
-          <label for="password"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="password" required>
-
-          <button type="submit">Login</button>
-          <label>
-            <input type="checkbox" checked="checked" name="remember"> Remember me
-          </label>
-        </div>
-        <div class="container" style="background-color:#f1f1f1">
-          <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-          <span class="psw">Forgot <a href="#">password?</a></span>
-        </div>
-      </form>
-    </div>
-    <script>
-      var modal = document.getElementById('id01');
-      window.onclick = function(event) {
-        if (event.target == modal){
-          modal.style.display = "none";
-        }
-      }
-    </script>
   </div>
+<div class="login">
+  <div id="id01" class="modal">
+    <form class="modal-content animate" action="/action_page.php" method="post">
+      <div class="imgcontainer">
+        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">%times;</span>
+        <img src="login_silhouette.png" alt="Avatar" class="avatar">
+      </div>
+      <div class="container">
+        <label for="username"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+
+        <button type="submit">Login</button>
+        <label>
+          <input type="checkbox" checked="checked" name="remember"> Remember me
+        </label>
+      </div>
+      <div class="container" style="background-color:#f1f1f1">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+        <span class="psw">Forgot <a href="#">password?</a></span>
+      </div>
+    </form>
   </div>
+  <script>
+  var modal = document.getElementById('id01');
+  window.onclick = function(event) {
+    if (event.target == modal){
+      modal.style.display = "none";
+    }
+  }
+  </script>
+</div>
+
