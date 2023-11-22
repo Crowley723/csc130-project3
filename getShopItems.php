788bc2aadd285479035a260b34f235eec532dbbe
Crowley723 <littlehill723@gmail.com>
2023-11-21 18:32:12 -0800

    load shop items from database dynamically using ajax

---
+++ getShopItems.php
@@ -0,0 +1,62 @@
+<?php
+$DBhostname = getenv("SQLHOSTNAME");
+$usersDB = getenv("CSC130DBNAME");
+$DBusername = getenv("CSC130DBUSER");
+$DBpassword = getenv("CSC130DBPASS");
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    ob_start();
+    //echo " Get Request!";
+    
+    try{
+        $databaseConnection = mysqli_connect($DBhostname, $DBusername, $DBpassword, $usersDB);
+        if ($databaseConnection ->connect_error) {
+            throw new Exception("Database Connection Error, Error No.: ".$databaseConnection->connect_errno." | ".$databaseConnection->connect_error);
+        }
+
+        $getShopItemsQuery = "SELECT * FROM `Shop`";
+        //echo " Before postResult";
+        //echo $getShopItemsQuery;
+        if($postsResult = $databaseConnection->query($getShopItemsQuery)){
+            //echo " Inside postResult";
+            $outputData = array();
+            while($row = $postsResult->fetch_assoc()){
+                $row_ID = $row["ID"];
+                $row_Name = $row["Name"];
+                $row_Description = $row["Description"];
+                $row_Image_Path = $row["Image Path"];
+                $row_QOH = $row["Quantity On Hand"];
+                $outputData[] = array(
+                    'ID' => $row_ID,
+                    'Name' => $row_Name,
+                    'Description' => $row_Description,
+                    'Image Path' => $row_Image_Path,
+                    'Quantity On Hand' => $row_QOH
+                );
+            }
+
+        
+        header('Content-Type: application/json');
+        echo json_encode($outputData, JSON_THROW_ON_ERROR);
+
+        }
+        //echo "After postResult";
+        $databaseConnection->close();
+        ob_flush();
+        exit();
+        
+    }catch(Exception $e){
+        echo "Internal Server Error: " . $e->getMessage();
+        ob_flush();
+        exit();
+    }
+        
+}else{
+    http_response_code(400);
+    ob_flush();
+    exit();
+}
+
+
+?>
--- shop.php
+++ shop.php
@@ -5,23 +5,88 @@
     <title>Shop</title>
     <?php include "./header.php" ?>
     <link rel="stylesheet" href="/design.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script>
+        
+jQuery(document).ready(function(){
+    getPosts();
+});
 
+function getPosts(){
+    $.ajax({
+        url:'/getShopItems.php',
+        type:'GET',
+        dataType: 'json',
+        success: function(data){
+            updatePage(data);
+        },
+        error: function(xhr, status, error){
+            console.log(status + ': ' + error);
+        }
+    });
+}
+
+function updatePage(data){
+    if(data && data.length > 0){  // Check if data is not null and has items
+        for(var i = data.length - 1; i >= 0; i--){
+            var item = data[i];
+        // <div class="grid-item">
+        //     <img class="item-image" src="/assets/cat-food.png" alt="Cat Food">
+        //     <div class="text-container">
+        //         <div class="item-name">Cat Food</div>
+        //         <div class="item-quantity">Quantity on Hand: 7</div>
+        //         <div class="item-description">The food that you give your cat. Duh</div>
+        //     </div>
+        // </div>
+            if(item.hasOwnProperty('ID') && 
+                item.hasOwnProperty('Name') && 
+                item.hasOwnProperty('Description') && 
+                item.hasOwnProperty('Image Path') &&
+                item.hasOwnProperty('Quantity On Hand')){
+
+                const gridItem = document.createElement("div");
+                gridItem.className = 'grid-item';
+                gridItem.id = item['ID'];
+                const gridContainer = document.getElementById('grid-container');
+                gridContainer.appendChild(gridItem);
+
+                const itemImage = document.createElement("img");
+                itemImage.src = item['Image Path']; // Make sure 'IMAGE PATH' property exists in your data
+                itemImage.className = 'item-image';
+                gridItem.appendChild(itemImage);                
+
+                const textContainer = document.createElement("div");
+                textContainer.className = 'text-container';
+                gridItem.appendChild(textContainer);
+
+                const itemName = document.createElement('div');
+                itemName.className = 'item-name';
+                itemName.innerText = item['Name'];
+                textContainer.appendChild(itemName);
+
+                const itemQuantity = document.createElement('div');
+                itemQuantity.className = 'item-quantity';
+                itemQuantity.innerText = "In stock: " + item['Quantity On Hand'];
+                textContainer.appendChild(itemQuantity);
+
+                const itemDescription = document.createElement('div');
+                itemDescription.className = 'item-description';
+                itemDescription.innerText = item['Description'];
+                textContainer.appendChild(itemDescription);
+
+            }
+        }
+    }
+}
+
+    </script>
     </head>
     <body>
     <div class="shop-header">
         <h1>The Shoppe!<h1>
     </div>
-    <div class="grid-container">
-        <div class="grid-item">
-            <p>Item 1</p>
-        </div>
-        <div class="grid-item">
-        <p>Item 2</p>
-        </div>
-        <div class="grid-item">
-        <p>Item 3</p>
-        </div>
-        <!-- Repeat the grid items as needed -->
+    <div class="grid-container" id="grid-container">
+        
     </div>
     </body>
 </html>
