8af66397703982bcbc283ee0095cdd36b3df3d88
Crowley723 <littlehill723@gmail.com>
2023-12-07 11:06:27 -0800

    remove duplicate css include

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
-    <link rel="stylesheet" href="/design.css">
     <?php include "./header.php" ?>
     
 </head>
--- shop.php
+++ shop.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Shop</title>
     <?php include "./header.php" ?>
-    <link rel="stylesheet" href="/design.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script>
         
